92ab81c1d9a8cdc3fd27653471bd1b768d743a1f
Christian Droulers <droulers.christian@gmail.com>
2022-12-21 22:05:12 -0500

    Add basic translation + switch menu

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,18 +1,63 @@
+import { useState, MouseEvent } from "react";
 import { useTranslation } from "react-i18next";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import MenuItem from "@mui/material/MenuItem";
+import Menu from "@mui/material/Menu";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
 import logo from "../../logo.png";
 
 import "./Header.styles.scss";
+import i18n from "../../initializers/i18next";
 
 export interface HeaderProps {}
 
 function Header(props: HeaderProps) {
   const [t] = useTranslation("Header");
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+
+  const handleMenu = (event: MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const changeLang = (lang: string) => {
+    i18n.changeLanguage(lang);
+  };
+
   return (
-    <header className="app-header">
-      <h1>
-        <img src={logo} alt="Mindful logo" /> {t("title")}
-      </h1>
-    </header>
+    <AppBar position="fixed" className="app-header">
+      <Toolbar style={{ minHeight: "auto" }}>
+        <IconButton
+          size="large"
+          edge="start"
+          color="inherit"
+          aria-label="menu"
+          sx={{ mr: 2 }}
+          aria-controls="menu-appbar"
+          aria-haspopup="true"
+          onClick={handleMenu}>
+          <MenuIcon />
+        </IconButton>
+        <Menu
+          id="menu-appbar"
+          anchorEl={anchorEl}
+          keepMounted
+          open={Boolean(anchorEl)}
+          onClose={handleClose}>
+          {i18n.language !== "fr" && <MenuItem onClick={() => changeLang("fr")}>Français</MenuItem>}
+          {i18n.language !== "en" && <MenuItem onClick={() => changeLang("en")}>English</MenuItem>}
+        </Menu>
+        <Typography variant="h6" component="h1" sx={{ flexGrow: 1 }}>
+          <img src={logo} alt="Mindful logo" /> {t("title")}
+        </Typography>
+      </Toolbar>
+    </AppBar>
   );
 }
 
--- src/initializers/i18next.ts
+++ src/initializers/i18next.ts
@@ -1,19 +1,28 @@
-import i18n from "i18next";
+import i18n, { InitOptions } from "i18next";
 import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
 
 import resources from "../locales";
 
+const options: InitOptions = {
+  resources,
+  debug: process.env.NODE_ENV === "development",
+  fallbackLng: ["en", "fr"],
+  supportedLngs: ["en", "fr"],
+  detection: {
+    order: ["localStorage", "navigator"],
+    lookupLocalStorage: "i18nextLng",
+    caches: ["localStorage"],
+    excludeCacheFor: ["cimode"],
+  },
+  interpolation: {
+    escapeValue: false, // react already safes from xss
+  },
+};
+
 i18n
   .use(initReactI18next) // passes i18n down to react-i18next
-  .init({
-    resources,
-    lng: "fr", // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
-    // you can use the i18n.changeLanguage function to change the language manually: https://www.i18next.com/overview/api#changelanguage
-    // if you're using a language detector, do not define the lng option
-
-    interpolation: {
-      escapeValue: false, // react already safes from xss
-    },
-  });
+  .use(LanguageDetector)
+  .init(options);
 
 export default i18n;
